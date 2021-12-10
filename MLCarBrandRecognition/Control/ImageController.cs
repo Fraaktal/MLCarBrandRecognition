68ab76463c9dbc01a468ccfb1393b6260b860fb0
Tom <t.ravanel@orange.fr>
2021-12-10 16:19:06 +0100

    [MLCBR] SignalR + Images

---
+++ MLCarBrandRecognition/Control/ImageController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Mime;
+using System.Threading.Tasks;
+
+namespace MLCarBrandRecognition.Control
+{
+    public class ImageController
+    {
+        public void SaveImage(byte[] byteArrayIn)
+        {
+            MemoryStream ms = new MemoryStream(byteArrayIn);
+            Image returnImage = Image.FromStream(ms);
+        }
+    }
+}
